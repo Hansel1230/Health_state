634dd822cba2bd0a30187953e2ee2c85b2f14c1c
Sweetb720x <ejarolsaul@gmail.com>
2025-07-02 20:06:40 -0400

    feat: crear queries de medico

---
+++ HealthState.Aplicacion/Medico/Queries/MedicoGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using HealthState.Aplicacion.Common.Models;
+using HealthState.Aplicacion.Medico.Models;
+using MediatR;
+
+namespace HealthState.Aplicacion.Medico.Queries
+{
+    public record MedicoGetAllQuery(string? Search, int Skip = 0, int Take = 10) : IRequest<PaginationResponseModel<MedicoModel>> { }
+}
---
+++ HealthState.Aplicacion/Medico/Queries/MedicoGetByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using HealthState.Aplicacion.Medico.Models;
+using MediatR;
+
+namespace HealthState.Aplicacion.Medico.Queries
+{
+    public record MedicoGetByIdQuery(int Id) : IRequest<MedicoModel> { }
+}
