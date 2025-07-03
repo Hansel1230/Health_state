a2668603d73c2099c840e1f5926ec5f2839fa258
Sweetb720x <ejarolsaul@gmail.com>
2025-07-03 01:37:23 -0400

    feat: agregar commands de rol

---
+++ HealthState.Aplicacion/Rol/Commands/RolUpdateCommand.cs
@@ -0,0 +1,13 @@
+﻿using HealthState.Aplicacion.Rol.Models;
+using MediatR;
+using System.Text.Json.Serialization;
+
+namespace HealthState.Aplicacion.Rol.Commands
+{
+    public class RolUpdateCommand : IRequest<RolModel>
+    {
+        [JsonIgnore] public int RolId { get; set; }
+
+        public string Nombre { get; set; } = null!;
+    }
+}
