6537d0d8914429d58d96563710efb10ab131c15c
Sweetb720x <ejarolsaul@gmail.com>
2025-07-03 01:37:39 -0400

    feat: agregar queries de rol

---
+++ HealthState.Aplicacion/Rol/Queries/RolGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using HealthState.Aplicacion.Rol.Models;
+using MediatR;
+
+namespace HealthState.Aplicacion.Rol.Queries
+{
+    public record RolGetAllQuery : IRequest<IEnumerable<RolModel>> { }
+
+}
