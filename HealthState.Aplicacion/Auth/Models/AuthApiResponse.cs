ad530454c05cb5748b7819b1e5cc174ebbadb5f5
Sweetb720x <ejarolsaul@gmail.com>
2025-07-03 19:21:29 -0400

    refactor: eliminar clases no utilizadas

--- HealthState.Aplicacion/Auth/Models/AuthApiResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HealthState.Aplicacion.Auth.Models
-{
-    public class AuthApiResponse<T>
-    {
-        public string Message { get; set; }
-        public T Result { get; set; }
-        public bool Completed { get; set; }
-    }
-}
