806fb44668e857882562a73fa658c5e14c48b249
Sweetb720x <ejarolsaul@gmail.com>
2025-07-03 01:38:09 -0400

    feat: agregar handlers de rol

---
+++ HealthState.Aplicacion/Rol/Handlers/RolGetAllQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using HealthState.Aplicacion.Common.Interfaces;
+using HealthState.Aplicacion.Common.Models;
+using HealthState.Aplicacion.Common.Utils;
+using HealthState.Aplicacion.Rol.Models;
+using HealthState.Aplicacion.Rol.Queries;
+using MediatR;
+
+namespace HealthState.Aplicacion.Rol.Handlers
+{
+    public class RolGetAllQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<RolGetAllQuery, IEnumerable<RolModel>>
+    {
+        public async Task<IEnumerable<RolModel>> Handle(RolGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var repository = unitOfWork.GetRepository<HealthState.Dominio.Role>();
+            
+            var query = PredicateBuilder.Build<HealthState.Dominio.Role>();
+
+            var data = await repository.GetAsync(
+                query,
+                orderBy: x => x.OrderByDescending(role => role.RolId));
+
+            return mapper.Map<IEnumerable<RolModel>>(data);
+        }
+    }
+}
---
+++ HealthState.Aplicacion/Rol/Handlers/RolGetByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HealthState.Aplicacion.Common.Exceptions;
+using HealthState.Aplicacion.Common.Interfaces;
+using HealthState.Aplicacion.Common.Resources;
+using HealthState.Aplicacion.Rol.Models;
+using HealthState.Aplicacion.Rol.Queries;
+using MediatR;
+
+namespace HealthState.Aplicacion.Rol.Handlers
+{
+    public class RolGetByIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<RolGetByIdQuery, RolModel>
+    {
+        public async Task<RolModel> Handle(RolGetByIdQuery request, CancellationToken cancellationToken)
+        {
+            var respository = unitOfWork.GetRepository<HealthState.Dominio.Role>();
+            var entity = await respository.FirstAsync(x => x.RolId == request.Id);
+
+            return entity == null
+                ? throw BusinessException.Instance(string.Format(MessageResource.EntityNotExist, request.Id))
+                : mapper.Map<RolModel>(entity);
+        }
+    }
+}
