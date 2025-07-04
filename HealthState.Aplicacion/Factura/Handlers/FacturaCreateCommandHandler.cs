257ae336dc578499e2af040a9f35787de1ec0d8f
Sweetb720x <ejarolsaul@gmail.com>
2025-07-04 11:37:56 -0400

    feat: agregar handlers de factura

---
+++ HealthState.Aplicacion/Factura/Handlers/FacturaCreateCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using HealthState.Aplicacion.Common.Exceptions;
+using HealthState.Aplicacion.Common.Interfaces;
+using HealthState.Aplicacion.Common.Resources;
+using HealthState.Aplicacion.Factura.Commands;
+using HealthState.Aplicacion.Factura.Models;
+using MediatR;
+
+namespace HealthState.Aplicacion.Factura.Handlers
+{
+    public class FacturaCreateCommandHandler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<FacturaCreateCommand, FacturaModel>
+    {
+        public async Task<FacturaModel> Handle(FacturaCreateCommand request, CancellationToken cancellationToken)
+        {
+            var repositoryPaciente = unitOfWork.GetRepository<HealthState.Dominio.Paciente>();
+
+            if (!await repositoryPaciente.ExistAsync(x => x.Cedula == request.Cedula))
+                throw NotFoundException.Instance(string.Format(MessageResource.EntityNotExist, request.Cedula));
+            
+            var entityPaciente = await repositoryPaciente.FirstAsync(x => x.Cedula == request.Cedula);
+
+            var repositoryFactura = unitOfWork.GetRepository<HealthState.Dominio.Factura>();
+
+            var entityFactura = mapper.Map<HealthState.Dominio.Factura>(request);
+
+            entityFactura.PacienteId = entityPaciente.PacienteId;
+            await repositoryFactura.InsertAsync(entityFactura);
+
+            await unitOfWork.SaveChangesAsync();
+
+            return mapper.Map<FacturaModel>(entityFactura);
+        }
+    }
+}
---
+++ HealthState.Aplicacion/Factura/Handlers/FacturaGetAllQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using HealthState.Aplicacion.Common.Interfaces;
+using HealthState.Aplicacion.Common.Models;
+using HealthState.Aplicacion.Common.Utils;
+using HealthState.Aplicacion.Factura.Models;
+using HealthState.Aplicacion.Factura.Queries;
+using MediatR;
+
+namespace HealthState.Aplicacion.Factura.Handlers
+{
+    public class FacturaGetAllQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<FacturaGetAllQuery, PaginationResponseModel<FacturaModel>>
+    {
+        public async Task<PaginationResponseModel<FacturaModel>> Handle(FacturaGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var repository = unitOfWork.GetRepository<HealthState.Dominio.Factura>();
+            var query = PredicateBuilder.Build<HealthState.Dominio.Factura>();
+
+            if (!string.IsNullOrEmpty(request.Search))
+                query = query.And(x => x.Paciente.Cedula.Equals(request.Search));
+
+            if (request.FechaInicio.HasValue)
+                query = query.And(x => x.FechaEmision >= request.FechaInicio.Value);
+
+            if (request.FechaFin.HasValue)
+                query = query.And(x => x.FechaEmision <= request.FechaFin.Value);
+
+            var totalCount = await repository.CountAsync(query); 
+
+            if (totalCount == 0)
+                return new PaginationResponseModel<FacturaModel>(Enumerable.Empty<FacturaModel>(), 0);
+
+            var data = await repository.GetAsync(query, orderBy: x => x.OrderByDescending(x => x.FacturaId),
+                skip: request.Skip, take: request.Take);
+
+            return new PaginationResponseModel<FacturaModel>(mapper.Map<IEnumerable<FacturaModel>>(data),
+                totalCount,
+                request.Skip,
+                request.Take);
+        }
+    }
+}
