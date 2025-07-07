4065ad6c6c38fa89aa8fc6de2ba1fe67182c84d0
Sweetb720x <ejarolsaul@gmail.com>
2025-07-07 01:20:39 -0400

    feat: crear queries detalle factura

---
+++ HealthState.Aplicacion/DetalleFactura/Queries/DetalleFacturaGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using HealthState.Aplicacion.Common.Models;
+using HealthState.Aplicacion.DetalleFactura.Models;
+using MediatR;
+
+namespace HealthState.Aplicacion.DetalleFactura.Queries
+{
+    public record DetalleFacturaGetAllQuery(string? Search, int Skip = 0, int Take = 10) : IRequest<PaginationResponseModel<DetalleFacturaModel>> { }
+}
---
+++ HealthState.Aplicacion/DetalleFactura/Queries/DetalleFacturaGetByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using HealthState.Aplicacion.DetalleFactura.Models;
+using MediatR;
+
+namespace HealthState.Aplicacion.DetalleFactura.Queries
+{
+    public record DetalleFacturaGetByIdQuery(int Id) : IRequest<DetalleFacturaModel> { }
+}
