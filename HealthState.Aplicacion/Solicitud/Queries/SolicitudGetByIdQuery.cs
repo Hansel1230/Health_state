3cb35c5423f23b317176ec5df251d61baab67ab2
Sweetb720x <ejarolsaul@gmail.com>
2025-07-08 00:27:21 -0400

    feat: crear queries de solicitud

---
+++ HealthState.Aplicacion/Solicitud/Queries/SolicitudGetByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using HealthState.Aplicacion.Solicitud.Models;
+using MediatR;
+
+namespace HealthState.Aplicacion.Solicitud.Queries
+{
+    public record SolicitudGetByIdQuery(int Id) : IRequest<SolicitudModel> { }
+}
