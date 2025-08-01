b1b275e09d1c02cee66d8383bd3392d6c90c2467
Sweetb720x <ejarolsaul@gmail.com>
2025-08-01 00:10:52 -0400

    feat: agregar actualizar estado solicitud

---
+++ HealthState.Dominio/Enum/EstadoEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HealthState.Dominio.Enum
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum EstadoEnum
+    {
+        A = 100,
+        R = 101,
+        P = 102,
+
+        E = 103,
+        C = 104,
+        F = 105
+    }
+}
