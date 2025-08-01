ce478b93e6bd6af19238d6aa4a3363ebddfe789d
Sweetb720x <ejarolsaul@gmail.com>
2025-08-01 00:07:44 -0400

    refactor: descomentar autorizacion

--- HealthState.Api/Controllers/FacturaController.cs
+++ HealthState.Api/Controllers/FacturaController.cs
@@ -9,7 +9,7 @@ namespace HealthState.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class FacturaController : ApiController
     {
         [HttpGet("all")]
